193929d8910fa93816b47bf169d9fba1c6e99e77
hsab <hsab@users.noreply.github.com>
2019-04-11 16:34:18 -0700

    fix error for OF .1

--- src/ofxDepthCompressedFrame.h
+++ src/ofxDepthCompressedFrame.h
@@ -10,30 +10,34 @@
 
 #include "ofPixels.h"
 
-class ofxDepthCompressedFrame {
-	struct DiffPixel{
-		unsigned short pos;
-		short value;
-	};
-public:
-	void allocate(int w, int h, bool isKeyFrame);
-	void setRegistration(float pixel_size, float distance);
-	ofPixels_<short> & getPixels();
-	const ofPixels_<short> & getPixels() const;
-	float getPixelSize();
-	float getDistance();
-	void setIsKeyFrame(bool isKeyFrame);
-	bool isKeyFrame() const;
+using namespace std;
 
-	const vector<short> & compressedData();
-	void fromCompressedData(const char* data, size_t len);
-	vector<DiffPixel> & getUncompressedDiff();
-private:
-	ofPixels_<short> pixels;
-	vector<short> compressed;
-	vector<DiffPixel> uncompressedDiff;
-	bool compressedDirty;
-	size_t compressedBytes;
+class ofxDepthCompressedFrame {
+   struct DiffPixel {
+      unsigned short pos;
+      short value;
+   };
+
+  public:
+   void allocate(int w, int h, bool isKeyFrame);
+   void setRegistration(float pixel_size, float distance);
+   ofPixels_<short>& getPixels();
+   const ofPixels_<short>& getPixels() const;
+   float getPixelSize();
+   float getDistance();
+   void setIsKeyFrame(bool isKeyFrame);
+   bool isKeyFrame() const;
+
+   const vector<short>& compressedData();
+   void fromCompressedData(const char* data, size_t len);
+   vector<DiffPixel>& getUncompressedDiff();
+
+  private:
+   ofPixels_<short> pixels;
+   vector<short> compressed;
+   vector<DiffPixel> uncompressedDiff;
+   bool compressedDirty;
+   size_t compressedBytes;
 };
 
 #endif /* OFXDEPTHCOMPRESSEDFRAME_H_ */
--- src/ofxDepthOctree.h
+++ src/ofxDepthOctree.h
@@ -9,61 +9,64 @@
 #define OFXDEPTHOCTREE_H_
 
 #include "ofConstants.h"
-#include "ofVec3f.h"
-#include "ofPixels.h"
 #include "ofMesh.h"
+#include "ofPixels.h"
+#include "ofVec3f.h"
 
-struct ExternalBuffer{
-	const char * buffer;
-	size_t size;
+using namespace std;
+
+struct ExternalBuffer {
+   const char* buffer;
+   size_t size;
 };
 
 class ofxDepthOctree;
 
-class ofxDepthVoxel{
-public:
-	float x, y, z, side;
-	vector<size_t> indices;
-	ofxDepthOctree * parent;
-	void allocate(int levels);
-	bool inside(const ofVec3f & v);
-	void divide(int levels);
-	void fillMesh(ofMesh & mesh, int level);
-	vector<ofxDepthVoxel> divisions;
-	void serialize(ofBuffer & repr,int level);
-	ExternalBuffer deserialize(const ExternalBuffer & repr, int levels);
+class ofxDepthVoxel {
+  public:
+   float x, y, z, side;
+   vector<size_t> indices;
+   ofxDepthOctree* parent;
+   void allocate(int levels);
+   bool inside(const ofVec3f& v);
+   void divide(int levels);
+   void fillMesh(ofMesh& mesh, int level);
+   vector<ofxDepthVoxel> divisions;
+   void serialize(ofBuffer& repr, int level);
+   ExternalBuffer deserialize(const ExternalBuffer& repr, int levels);
 
-	bool diff(const ofxDepthVoxel & in, ofxDepthVoxel & diff);
-	bool isLeaf() const;
+   bool diff(const ofxDepthVoxel& in, ofxDepthVoxel& diff);
+   bool isLeaf() const;
 
-private:
-	void createDivisions();
-	ofVec2f worldToCamera(float x, float y, float z);
-	ofVec2f worldToCamera(const ofVec3f & p);
+  private:
+   void createDivisions();
+   ofVec2f worldToCamera(float x, float y, float z);
+   ofVec2f worldToCamera(const ofVec3f& p);
 };
 
 class ofxDepthOctree {
-public:
-	ofxDepthOctree();
-	virtual ~ofxDepthOctree();
+  public:
+   ofxDepthOctree();
+   virtual ~ofxDepthOctree();
 
-	void setRegistration(float pixel_size, float distance);
-	void allocate(ofShortPixels & depth, int levels);
-	void update(ofShortPixels & depth, int levels);
-	bool isAllocated();
-	ofMesh mesh(int level);
-	ofBuffer serialize(int level);
-	void deserialize(const ofBuffer & repr, int levels);
+   void setRegistration(float pixel_size, float distance);
+   void allocate(ofShortPixels& depth, int levels);
+   void update(ofShortPixels& depth, int levels);
+   bool isAllocated();
+   ofMesh mesh(int level);
+   ofBuffer serialize(int level);
+   void deserialize(const ofBuffer& repr, int levels);
 
-	void diff(const ofxDepthOctree & in, ofxDepthOctree & diff);
-private:
-	ofVec3f cameraToWorld(int x, int y, int z);
-	ofxDepthVoxel octree;
-	float pixel_size;
-	float distance;
-	vector<ofVec3f> pc;
-	float factor;
-	friend class ofxDepthVoxel;
+   void diff(const ofxDepthOctree& in, ofxDepthOctree& diff);
+
+  private:
+   ofVec3f cameraToWorld(int x, int y, int z);
+   ofxDepthVoxel octree;
+   float pixel_size;
+   float distance;
+   vector<ofVec3f> pc;
+   float factor;
+   friend class ofxDepthVoxel;
 };
 
 #endif /* OFXDEPTHOCTREE_H_ */
